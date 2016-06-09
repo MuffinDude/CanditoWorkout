8ba959a366faecd36201b56f133de9bff1e7ec0b
MuffinDude <joosep.tiismus@gmail.com>
2016-06-09 16:27:45 +0300

    Updated Schedules, stopper for most of the first week works, implemented color change for day 3 stopper, orientation is only portrait.

--- app/src/main/java/longerinoentertainment/canditoworkout/Week1/Day3.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/Week1/Day3.java
@@ -1,19 +1,41 @@
 package longerinoentertainment.canditoworkout.Week1;
 
+import android.graphics.Color;
+import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 import longerinoentertainment.canditoworkout.R;
 
 public class Day3 extends Fragment {
 
+    Button stopper;
+    Chronometer chronometer;
+    private long timeWhenStopped = 0;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View infoTab = inflater.inflate(R.layout.activity_day3, container, false);
+
+        stopper = (Button) infoTab.findViewById(R.id.stopperButton);
+        chronometer = (Chronometer) infoTab.findViewById(R.id.chronometer);
+
+        stopper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+                chronometer.start();
+                stopper.setBackgroundColor(Color.parseColor("#ff0000"));
+            }
+        });
+
         return infoTab;
     }
+    //changeColor(){}
 }
--- app/src/main/java/longerinoentertainment/canditoworkout/Week1/Day4.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/Week1/Day4.java
@@ -1,19 +1,37 @@
 package longerinoentertainment.canditoworkout.Week1;
 
+import android.os.SystemClock;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
 
 import longerinoentertainment.canditoworkout.R;
 
 public class Day4 extends Fragment {
+    Button stopper;
+    Chronometer chronometer;
+    private long timeWhenStopped = 0;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View infoTab = inflater.inflate(R.layout.activity_day4, container, false);
+
+        stopper = (Button) infoTab.findViewById(R.id.stopperButton);
+        chronometer = (Chronometer) infoTab.findViewById(R.id.chronometer);
+
+        stopper.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                chronometer.setBase(SystemClock.elapsedRealtime() + timeWhenStopped);
+                chronometer.start();
+            }
+        });
+
         return infoTab;
     }
 }
