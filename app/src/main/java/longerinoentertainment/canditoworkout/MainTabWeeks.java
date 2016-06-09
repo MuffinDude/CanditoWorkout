204fd5d092dd15f99cb8d06ce35628a6efd1d737
MuffinDude <joosep.tiismus@gmail.com>
2016-06-09 20:24:12 +0300

    Updated design for WeeksTab and Exercises. First and Second week done without functionality

--- app/src/main/java/longerinoentertainment/canditoworkout/MainTabWeeks.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/MainTabWeeks.java
@@ -9,21 +9,37 @@ import android.view.ViewGroup;
 import android.widget.Button;
 
 import longerinoentertainment.canditoworkout.Week1.Week1;
+import longerinoentertainment.canditoworkout.Week2.Week2;
 
 
 public class MainTabWeeks extends Fragment {
     public Button week1;
+    public Button week2;
+    public Button week3;
+    public Button week4;
+    public Button week5;
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View weekTab = inflater.inflate(R.layout.activity_main_tab_weeks, container, false);
 
         week1 = (Button) weekTab.findViewById(R.id.week1Button);
+        week2 = (Button) weekTab.findViewById(R.id.week2Button);
+        week3 = (Button) weekTab.findViewById(R.id.week3Button);
+        week4 = (Button) weekTab.findViewById(R.id.week4Button);
+        week5 = (Button) weekTab.findViewById(R.id.week5Button);
         week1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
                 Intent i = new Intent(getActivity(), Week1.class);
+                startActivity(i);
+            }
+        });
+        week2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), Week2.class);
                 startActivity(i);
             }
         });
---
+++ app/src/main/java/longerinoentertainment/canditoworkout/Week2/Day1.java
@@ -0,0 +1,18 @@
+package longerinoentertainment.canditoworkout.Week2;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import longerinoentertainment.canditoworkout.R;
+
+public class Day1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        final View infoTab = inflater.inflate(R.layout.activity_day12, container, false);
+        return infoTab;
+    }
+}
