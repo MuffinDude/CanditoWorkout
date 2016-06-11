2b2abac7c85841ba19af5e6296325e4d276b282c
MuffinDude <joosep.tiismus@gmail.com>
2016-06-12 01:27:24 +0300

    added toast for week 2 day 1 squat

--- app/src/main/java/longerinoentertainment/canditoworkout/Week2/Day1.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/Week2/Day1.java
@@ -5,14 +5,28 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import longerinoentertainment.canditoworkout.R;
 
 public class Day1 extends Fragment {
 
+    ImageButton info;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View infoTab = inflater.inflate(R.layout.activity_day12, container, false);
+
+        info = (ImageButton) infoTab.findViewById(R.id.infoButton);
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),
+                        "Still complete the 5 sets of 3 reps regardless even if you do perform less than 8 reps on the MR10 set", Toast.LENGTH_LONG).show();
+            }
+        });
+
         return infoTab;
     }
 }
--- app/src/main/java/longerinoentertainment/canditoworkout/Week2/Day2.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/Week2/Day2.java
@@ -6,14 +6,20 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import longerinoentertainment.canditoworkout.R;
 
+
 public class Day2 extends Fragment {
+
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View infoTab = inflater.inflate(R.layout.activity_day22, container, false);
+
         return infoTab;
     }
 }
