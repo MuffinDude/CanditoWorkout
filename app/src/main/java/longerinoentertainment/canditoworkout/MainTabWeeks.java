3fdf4fd4e068fdb89bbea1f4172e8172ccd1f7a5
MuffinDude <joosep.tiismus@gmail.com>
2016-06-11 18:53:28 +0300

    Week3 made

--- app/src/main/java/longerinoentertainment/canditoworkout/MainTabWeeks.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/MainTabWeeks.java
@@ -10,6 +10,10 @@ import android.widget.Button;
 
 import longerinoentertainment.canditoworkout.Week1.Week1;
 import longerinoentertainment.canditoworkout.Week2.Week2;
+import longerinoentertainment.canditoworkout.Week3.Week3;
+import longerinoentertainment.canditoworkout.Week4.Week4;
+import longerinoentertainment.canditoworkout.Week5.Week5;
+import longerinoentertainment.canditoworkout.Week6.Week6;
 
 
 public class MainTabWeeks extends Fragment {
@@ -18,6 +22,7 @@ public class MainTabWeeks extends Fragment {
     public Button week3;
     public Button week4;
     public Button week5;
+    public Button week6;
 
 
     @Override
@@ -29,6 +34,7 @@ public class MainTabWeeks extends Fragment {
         week3 = (Button) weekTab.findViewById(R.id.week3Button);
         week4 = (Button) weekTab.findViewById(R.id.week4Button);
         week5 = (Button) weekTab.findViewById(R.id.week5Button);
+        week6 = (Button) weekTab.findViewById(R.id.week6Button);
         week1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -40,6 +46,34 @@ public class MainTabWeeks extends Fragment {
             @Override
             public void onClick(View view) {
                 Intent i = new Intent(getActivity(), Week2.class);
+                startActivity(i);
+            }
+        });
+        week3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), Week3.class);
+                startActivity(i);
+            }
+        });
+        week4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), Week4.class);
+                startActivity(i);
+            }
+        });
+        week5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), Week5.class);
+                startActivity(i);
+            }
+        });
+        week6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(getActivity(), Week6.class);
                 startActivity(i);
             }
         });
