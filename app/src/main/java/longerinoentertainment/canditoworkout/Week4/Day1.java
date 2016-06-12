4d08eb20a6d24512cfb81796e0075c3fa8496d81
MuffinDude <joosep.tiismus@gmail.com>
2016-06-12 22:33:20 +0300

    Week 4 calculable

--- app/src/main/java/longerinoentertainment/canditoworkout/Week4/Day1.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/Week4/Day1.java
@@ -6,14 +6,63 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import longerinoentertainment.canditoworkout.R;
 
 public class Day1 extends Fragment {
+    Button squat1;
+    Button squat2;
+    Button squat3;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View infoTab = inflater.inflate(R.layout.activity_day14, container, false);
+
+        squat1 = (Button) infoTab.findViewById(R.id.squatText1);
+        squat2 = (Button) infoTab.findViewById(R.id.squatText2);
+        squat3 = (Button) infoTab.findViewById(R.id.squatText3);
+
+
+        final File dir = new File(getContext().getFilesDir() + "/CanditoWorkoutApp");
+        final File file = new File(dir, "savedFile.txt");
+        String[] values = readFromFile(new File(dir, "savedFile.txt"));
+        readFromFile(file);
+
+        double squatNumber = round(values[1]);
+
+        String squatText1 = Double.toString(squatNumber+5) + "x3";
+        String squatText2 = Double.toString(squatNumber+7.5) + "x3";
+        String squatText3 = Double.toString(squatNumber+10) + "x3";
+        squat1.setText(squatText1);
+        squat2.setText(squatText2);
+        squat3.setText(squatText3);
         return infoTab;
+    }
+
+    public String[] readFromFile(File file){
+        String[] values = new String[3];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            int i = 0;
+            while ((line = br.readLine()) != null){
+                values[i] = line;
+                i++;
+            }
+            br.close();
+        }
+        catch (IOException ignored){
+        }
+        return values;
+    }
+    public static double round(String valueString) {
+        double value = Math.round(Math.round(Double.parseDouble(valueString)/2.5)*2.5 * 0.8/2.5)*2.5;
+        return value;
     }
 }
--- app/src/main/java/longerinoentertainment/canditoworkout/Week4/Day3.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/Week4/Day3.java
@@ -6,14 +6,68 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import longerinoentertainment.canditoworkout.R;
 
 public class Day3 extends Fragment {
 
+    Button squat1;
+    Button squat2;
+    Button dead1;
+    Button dead2;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View infoTab = inflater.inflate(R.layout.activity_day34, container, false);
+
+        squat1 = (Button) infoTab.findViewById(R.id.squatText1);
+        squat2 = (Button) infoTab.findViewById(R.id.squatText2);
+        dead1 = (Button) infoTab.findViewById(R.id.deadText1);
+        dead2 = (Button) infoTab.findViewById(R.id.deadText2);
+
+        final File dir = new File(getContext().getFilesDir() + "/CanditoWorkoutApp");
+        final File file = new File(dir, "savedFile.txt");
+        String[] values = readFromFile(new File(dir, "savedFile.txt"));
+        readFromFile(file);
+
+        double deadNumber = round(values[2]);
+        double squatNumber = round(values[1]);
+
+        String deadText1 = Double.toString(deadNumber+15)+ "x3";
+        String deadText2 = Double.toString(deadNumber+17.5)+ "x1-2";
+        String squatText1 = Double.toString(squatNumber+10) + "x3";
+        String squatText2 = Double.toString(squatNumber+12.5) + "x1-2";
+        squat1.setText(squatText1);
+        squat2.setText(squatText2);
+        dead1.setText(deadText1);
+        dead2.setText(deadText2);
         return infoTab;
+    }
+
+    public String[] readFromFile(File file){
+        String[] values = new String[3];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            int i = 0;
+            while ((line = br.readLine()) != null){
+                values[i] = line;
+                i++;
+            }
+            br.close();
+        }
+        catch (IOException ignored){
+        }
+        return values;
+    }
+    public static double round(String valueString) {
+        double value = Math.round(Math.round(Double.parseDouble(valueString)/2.5)*2.5 * 0.8/2.5)*2.5;
+        return value;
     }
 }
