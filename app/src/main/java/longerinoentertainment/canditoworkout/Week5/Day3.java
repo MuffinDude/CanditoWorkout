193953e10a367f611f20e0868336dee0dc15d449
MuffinDude <joosep.tiismus@gmail.com>
2016-06-12 22:42:36 +0300

    Week 5 calculable

--- app/src/main/java/longerinoentertainment/canditoworkout/Week5/Day3.java
+++ app/src/main/java/longerinoentertainment/canditoworkout/Week5/Day3.java
@@ -6,14 +6,54 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 
 import longerinoentertainment.canditoworkout.R;
 
 public class Day3 extends Fragment {
+    Button dead1;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final View infoTab = inflater.inflate(R.layout.activity_day35, container, false);
+
+        dead1 = (Button) infoTab.findViewById(R.id.deadText1);
+
+        final File dir = new File(getContext().getFilesDir() + "/CanditoWorkoutApp");
+        final File file = new File(dir, "savedFile.txt");
+        String[] values = readFromFile(new File(dir, "savedFile.txt"));
+        readFromFile(file);
+
+        double deadNumber = round(values[2]);
+
+        String deadText1 = Double.toString(deadNumber+20)+ "x1-4";
+        dead1.setText(deadText1);
         return infoTab;
+    }
+
+    public String[] readFromFile(File file){
+        String[] values = new String[3];
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            String line;
+            int i = 0;
+            while ((line = br.readLine()) != null){
+                values[i] = line;
+                i++;
+            }
+            br.close();
+        }
+        catch (IOException ignored){
+        }
+        return values;
+    }
+    public static double round(String valueString) {
+        double value = Math.round(Math.round(Double.parseDouble(valueString)/2.5)*2.5 * 0.8/2.5)*2.5;
+        return value;
     }
 }
